d6b88ec4bc0bd119698bc5fc49efa26602f32120
FLOPS LLC <ops@floatingpoint.cloud>
2025-09-28 10:23:26 -0400

    fixed error handling

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import path from "path";
+import { readFileSync as readFile } from "fs";
 import * as sass from "sass";
 import { transform } from 'lightningcss'; //provided by bodykit
 
@@ -19,10 +20,10 @@ export default class extends Compiler {
 
 	async build( props ) {
 
-		try {
+		const debugOut = this.out( path.dirname( props.filepath ), path.basename( props.filename, '.scss' ), '.debug.css', this.compiledFilename ?? props.filenamePattern ?? this.filenamePattern );
+		const minifOut = this.out( path.dirname( props.filepath ), path.basename( props.filename, '.scss' ), '.min.css', this.minifiedFilename ?? props.filenamePattern ?? this.filenamePattern  );
 
-			const debugOut = this.out( path.dirname( props.filepath ), path.basename( props.filename, '.scss' ), '.debug.css', this.compiledFilename ?? props.filenamePattern ?? this.filenamePattern );
-			const minifOut = this.out( path.dirname( props.filepath ), path.basename( props.filename, '.scss' ), '.min.css', this.minifiedFilename ?? props.filenamePattern ?? this.filenamePattern  );
+		try {
 
 			const result = await sass.compileAsync( props.in, {
 				sourceMap: true,
@@ -76,20 +77,62 @@ export default class extends Compiler {
 
 		} catch( error ) {
 
+			const errorMessage = [
+				`\x1b[31mError compiling SCSS for \x1b[4m${path.relative(props.sourceIn ?? this.sourceIn, props.in)}`,
+			];
+
+			if ( error.hasOwnProperty('_dartException') ) {
+				//sass error
+				const stackTop = error._dartException.trace.frames[0];
+				const tracedSourcePath = path.relative( props.sourceIn ?? this.sourceIn, decodeURI( stackTop.uri.path ) );
+				const tracedSourceContents = readFile( decodeURI( stackTop.uri.path ), 'utf8' );
+				errorMessage.push(`dartsass: ${error.sassMessage}, \x1b[4m${tracedSourcePath}[${stackTop.line}:${stackTop.column}]` );
+				errorMessage.push( ...this.getSourceContext( tracedSourceContents, stackTop.line, stackTop.column ) );
+
+			} else if ( error.hasOwnProperty('source') && ( error.loc?.line ?? false ) ) {
+				//lightningcss error, need to figure out line numbers of originals
+				errorMessage.push(` lightningcss: ${error.data?.type ?? 'SyntaxError'}: ${error.message}, \x1b[4m${debugOut} [${error.loc.line}:${error.loc.column}]`);
+				errorMessage.push( ...this.getSourceContext( error.source, error.loc.line, error.loc.column ) );
+
+			}
+
+			if ( errorMessage.length > 1 ) {
+
+				return [
+					{
+						...props,
+						error: errorMessage.join('\x1b[0m\n'),
+					}
+				]
+			}
+
 			return [ {
 				...props,
-				error: {
-					type: error.data?.type ?? error.name,
-					line: error.loc?.line ?? error.cause?.line ?? null,
-					column: error.loc?.column ?? error.cause?.column ?? null,
-					path: `.${error.fileName?.replace( this.sourceIn, '' ) ?? ''}`,
-					message: error.message,
-					stack: error.stack ?? null,
-				}
+				error
 			} ];
 
 		}
 
 	}
 
+	getSourceContext( source, line, column, linesBefore = 1, linesAfter = 1) {
+		const lines = source.split('\n');
+		const start = Math.max( 0, line - linesBefore - 1);
+		const end = Math.min( lines.length, line + linesAfter);
+		const context = [];
+		lines.slice( start, end ).forEach( ( thisLine, i ) => {
+			const current = start + i + 1;
+			if ( current !== line ) {
+				context.push(`\x1b[2m${current}:\x1b[0m ${thisLine}`);
+				return;
+			}
+			context.push(
+				`\x1b[1m\x1b[2m${current}:\x1b[0m `
+				+ thisLine.substring(0, column - 1)
+				+ `\x1b[4m\x1b[31m` + thisLine.substring( column - 1, column) + `\x1b[0m`
+				+ thisLine.substring( column )
+			);
+		});
+		return context;
+	}
 }
