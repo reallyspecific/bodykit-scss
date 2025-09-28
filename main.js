ed847726f68b4253ad75efa98d0ddf95a0e41753
FLOPS LLC <ops@floatingpoint.cloud>
2025-09-28 10:23:27 -0400

    bug fixes found during usage, better error messages on scss module

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import path from "path";
 import { readFileSync as readFile } from "fs";
+import {URL} from "url";
 import * as sass from "sass";
 import { transform } from 'lightningcss'; //provided by bodykit
+import terminalLink from 'terminal-link'; //provided by bodykit
 
 import Compiler from "@reallyspecific/bodykit/compiler";
 
@@ -25,21 +27,32 @@ export default class extends Compiler {
 
 		try {
 
-			const result = await sass.compileAsync( props.in, {
+			const debugOutput = await sass.compileAsync( props.in, {
 				sourceMap: true,
 				sourceMapIncludeSources: true,
 				quietDeps: true,
 				...( props.options ?? this.options ?? {} )
 			});
 
-			let { code, map } = await transform(
+			let minifiedOutput = await transform(
 				{
-					code: Buffer.from( result.css ),
+					code: Buffer.from( debugOutput.css ),
 					targets: props.targets ?? this.targets,
 					minify: true,
 					sourceMap: true
 				}
-			)
+			);
+
+			const debugSourceMap = { ...debugOutput.sourceMap };
+			debugSourceMap.sourceRoot = path.dirname( path.relative( props.out, props.sourceIn ?? this.sourceIn ) );
+			debugSourceMap.sources = debugOutput.sourceMap.sources.map( source => {
+				const sourcePath = decodeURI( source ).replace('file://','');
+				return path.relative( props.sourceIn ?? this.sourceIn, sourcePath );
+			} );
+
+			const minifiedSourceMap = JSON.parse( minifiedOutput.map );
+			minifiedSourceMap.sourceRoot = '.';
+			minifiedSourceMap.sources = [ path.basename( debugOut ) ];
 
 			const files = [ {
 				...props,
@@ -47,28 +60,28 @@ export default class extends Compiler {
 				filepath: debugOut,
 				filename: path.basename( debugOut ),
 				ext: path.extname( debugOut ),
-				contents: result.css,
+				contents: debugOutput.css,
 			},{
 				...props,
 				out: path.join( props.destOut ?? this.destOut, debugOut ) + '.map',
 				filepath: debugOut + '.map',
 				filename: path.basename( debugOut ) + '.map',
 				ext: '.map',
-				contents: JSON.stringify( result.sourceMap ),
+				contents: JSON.stringify( debugSourceMap ),
 			},{
 				...props,
 				out: path.join( props.destOut ?? this.destOut, minifOut ),
 				filepath: minifOut,
 				filename: path.basename( minifOut ),
 				ext: path.extname( minifOut ),
-				contents: code,
+				contents: minifiedOutput.code,
 			},{
 				...props,
 				out: path.join( props.destOut ?? this.destOut, minifOut ) + '.map',
 				filepath: minifOut + '.map',
 				filename: path.basename( minifOut ) + '.map',
 				ext: '.map',
-				contents: map,
+				contents: JSON.stringify( minifiedSourceMap ),
 			} ];
 
 			this.collection.add( ...files );
@@ -82,12 +95,22 @@ export default class extends Compiler {
 			];
 
 			if ( error.hasOwnProperty('_dartException') ) {
-				//sass error
-				const stackTop = error._dartException.trace.frames[0];
-				const tracedSourcePath = path.relative( props.sourceIn ?? this.sourceIn, decodeURI( stackTop.uri.path ) );
-				const tracedSourceContents = readFile( decodeURI( stackTop.uri.path ), 'utf8' );
-				errorMessage.push(`dartsass: ${error.sassMessage}, \x1b[4m${tracedSourcePath}[${stackTop.line}:${stackTop.column}]` );
-				errorMessage.push( ...this.getSourceContext( tracedSourceContents, stackTop.line, stackTop.column ) );
+				errorMessage.push(`dartsass: ${error.sassMessage}`);
+				if ( error._dartException?.trace?.frames ) {
+
+					error._dartException.trace.frames.forEach( ( trace, index ) => {
+						const tracePath = path.relative(props.sourceIn ?? this.sourceIn, decodeURI(trace.uri.path));
+						if ( index === 0 ) {
+							errorMessage.push(`\x1b[2m${tracePath}[${trace.line}:${trace.column}]\x1b[0m`);
+							const traceSource = readFile(decodeURI(trace.uri.path), 'utf8');
+							errorMessage.push(...this.getSourceContext(traceSource, trace.line, trace.column));
+						} else {
+							errorMessage.push(`\x1b[2m ↳ ${tracePath}[${trace.line}:${trace.column}]\x1b[0m`);
+						}
+					} )
+				} else {
+					errorMessage.push(`dartsass: ${error.message}`);
+				}
 
 			} else if ( error.hasOwnProperty('source') && ( error.loc?.line ?? false ) ) {
 				//lightningcss error, need to figure out line numbers of originals
